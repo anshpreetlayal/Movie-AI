b31c398c98418e2998038e56d2e61e1525076b4a
Anshpreet Kaur <anshpreetlayal@outlook.com>
2023-11-28 18:27:54 -0500

    edit

--- Components/LoadingSpinner.js
+++ Components/LoadingSpinner.js
@@ -13,5 +13,14 @@ const SpinnerContainer = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-  height: 100px; /* Adjust the height as needed */
+  height: 100px;
+`;
+
+const Spinner = styled.div`
+  border: 4px solid rgba(0, 0, 0, 0.1);
+  border-top: 4px solid #333;
+  border-radius: 50%;
+  width: 30px;
+  height: 30px;
+  animation: ${spinAnimation} 1s linear infinite;
 `;
