6dddd6f2a7abcce4293b409f0d715da27889529c
Anshpreet Kaur <anshpreetlayal@outlook.com>
2023-11-28 18:49:06 -0500

    editing

--- Components/MovieList/MovieItem.js
+++ Components/MovieList/MovieItem.js
@@ -4,5 +4,19 @@ function Login({ onLogin }) {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
-  
+
+  const handleLogin = () => {
+    // Basic validation as if yet, will implement a stronger validation
+    if (username === 'example_user' && password === 'password123') {
+      onLogin(username); // Pass the username to the parent component or function
+      setUsername('');
+      setPassword('');
+      setError('');
+    } else {
+      setError('Invalid username or password');
+    }
+  };
+
 }
+
+export default Login;
