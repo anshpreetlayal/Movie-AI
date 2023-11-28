0d4eea055771630893d7c060ae42fc90eaeaefcf
Anshpreet Kaur <anshpreetlayal@outlook.com>
2023-11-28 18:24:29 -0500

    edit

--- Components/SearchBar.js
+++ Components/SearchBar.js
@@ -5,5 +5,18 @@ function SearchBar ({onSearch}) {
 
     const handleSearch = ( ) => {
         onSearch (searchTerm);
-    }
+    };
+
+    return (
+        <div> 
+            <input 
+            type = "text"
+            value={searchTerm}
+            onChange= {e => setSearchTerm(e.target.value)}
+            />
+            <button onClick={handleSearch}>Search</button>
+        </div>
+    );
 }
+
+export default SearchBar;
