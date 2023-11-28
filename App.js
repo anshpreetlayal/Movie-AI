773db9746a680ba595879e4113f00be61ca3869b
Anshpreet Kaur <anshpreetlayal@outlook.com>
2023-11-28 18:48:22 -0500

    editing

--- App.js
+++ App.js
@@ -4,8 +4,9 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text> </Text>
       <StatusBar style="auto" />
+      
     </View>
   );
 }
---
+++ Components/MovieList/MovieItem.js
@@ -0,0 +1,8 @@
+import React, { useState } from 'react';
+
+function Login({ onLogin }) {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+  
+}
