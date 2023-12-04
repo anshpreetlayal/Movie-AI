98ca565928058038fbe5444a3ba8612e7e0cd1c7
Anshpreet Kaur <anshpreetlayal@outlook.com>
2023-12-04 18:53:21 -0500

    edit

--- Components/UserProfile.js
+++ Components/UserProfile.js
@@ -4,8 +4,15 @@ function UserProfile({ username, email, avatar }) {
   return (
     <div className="user-profile">
       <h2>User Profile</h2>
-
+      <div className="user-info">
+        <img src={avatar} alt="User Avatar" />
+        <div>
+          <p><strong>Username:</strong> {username}</p>
+          <p><strong>Email:</strong> {email}</p>
+          {/* Add more user information or options as needed */}
+        </div>
       </div>
+    </div>
   );
 }
 
