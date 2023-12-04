cd60a0bbddb730f4d91e21aa2246eb9c6e7ad1a8
Anshpreet Kaur <anshpreetlayal@outlook.com>
2023-12-04 18:52:28 -0500

    edits

--- App.js
+++ App.js
@@ -11,3 +11,30 @@ function App() {
     fetchMovies();
   }, []);
 
+  const fetchMovies = async () => {
+    try {
+      const response = await fetch(
+        'https://api.themoviedb.org/3/movie/popular?api_key=50758310d2affb15cd0100230ef8ab9dY'
+      );
+      const data = await response.json();
+      setMovies(data.results);
+      setLoading(false);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div className="App">
+      <Header />
+      {loading ? (
+        <LoadingSpinner />
+      ) : (
+        <MovieList movies={movies} />
+      )}
+    </div>
+  );
+}
+
+export default App;
---
+++ Components/UserProfile.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function UserProfile({ username, email, avatar }) {
+  return (
+    <div className="user-profile">
+      <h2>User Profile</h2>
+
+      </div>
+  );
+}
+
+export default UserProfile;
