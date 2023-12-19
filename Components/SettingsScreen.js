e20225a941aa94124b6a31bb6f66fdea83f2dd2a
Anshpreet Kaur <anshpreetlayal@outlook.com>
2023-12-19 18:18:00 -0500

    editing

--- Components/SettingsScreen.js
+++ Components/SettingsScreen.js
@@ -54,7 +54,43 @@ const styles = StyleSheet.create({
       backgroundColor: "#ffffff",
       padding: 20,
     },
-  
+    containerDark: {
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "center",
+      backgroundColor: "#333333",
+      padding: 20,
+    },
+    title: {
+      fontSize: 24,
+      fontWeight: "bold",
+      marginBottom: 20,
+      color: "#000000",
+    },
+    optionContainer: {
+      flexDirection: "row",
+      alignItems: "center",
+      justifyContent: "space-between",
+      marginBottom: 20,
+      width: "100%",
+    },
+    optionTitle: {
+      fontSize: 18,
+      color: "#000000",
+    },
+    optionButton: {
+      backgroundColor: "#6B9362",
+      paddingVertical: 12,
+      paddingHorizontal: 20,
+      borderRadius: 8,
+      marginBottom: 10,
+      width: "100%",
+      alignItems: "center",
+    },
+    optionButtonText: {
+      fontSize: 16,
+      color: "#ffffff",
+    },
   });
   
 
