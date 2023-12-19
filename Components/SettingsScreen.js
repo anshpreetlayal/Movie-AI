cb3fb582feb338d326a2067376706339b8ceb9ae
Anshpreet Kaur <anshpreetlayal@outlook.com>
2023-12-19 18:07:46 -0500

    editing

--- Components/SettingsScreen.js
+++ Components/SettingsScreen.js
@@ -15,6 +15,33 @@ const SettingsScreen = ({ navigation }) => {
     setReceiveNotifications((prev) => !prev);
   };
 
+// const logout
+//const useraccount
+
+return (
+    <View style={isDarkMode ? styles.containerDark : styles.containerLight}>
+      <Text style={styles.title}>Settings</Text>
+
+      <View style={styles.optionContainer}>
+        <Text style={styles.optionTitle}>Dark Mode</Text>
+        <Switch value={isDarkMode} onValueChange={toggleDarkMode} />
+      </View>
+
+      <View style={styles.optionContainer}>
+        <Text style={styles.optionTitle}>Receive Notifications</Text>
+        <Switch value={receiveNotifications} onValueChange={toggleNotifications} />
+      </View>
+      
+
+      <TouchableOpacity
+        style={styles.optionButton}
+        onPress={() => {
+        }}
+      >
+        <Text style={styles.optionButtonText}>Additional Setting</Text>
+      </TouchableOpacity>
+    </View>
+  );
 
 };
 
