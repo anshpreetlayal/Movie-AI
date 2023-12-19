88b5dd9fc44cc07774e9bb4f03229fc3743e07c3
Anshpreet Kaur <anshpreetlayal@outlook.com>
2023-12-19 18:05:51 -0500

    editing

--- Components/SettingsScreen.js
+++ Components/SettingsScreen.js
@@ -2,5 +2,14 @@ import React, { useState } from "react";
 import { View, Text, StyleSheet, Switch, TouchableOpacity } from "react-native";
 
 
+const SettingsScreen = ({ navigation }) => {
+    const [isDarkMode, setIsDarkMode] = useState(false);
+    const [receiveNotifications, setReceiveNotifications] = useState(true);
+  
+    const toggleDarkMode = () => {
+      setIsDarkMode((prevMode) => !prevMode);
+    };
+
+}
 
 export default SettingsScreen;
