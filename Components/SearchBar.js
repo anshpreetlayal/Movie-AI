cd0044db6552d732c79f1af26416355bf689bbcc
Anshpreet Kaur <anshpreetlayal@outlook.com>
2023-12-28 11:40:48 -0500

    updated searchBar

--- Components/SearchBar.js
+++ Components/SearchBar.js
@@ -6,5 +6,28 @@ function SearchBar(){
     const handleSearchClick = () => {
         setShowSearch(!showSearch);
       };
+      return (
+        <div style={{ textAlign: 'center', marginTop: '20px' }}>
+          <button onClick={handleSearchClick} style={{ fontSize: '24px' }}>
+            🔍
+          </button>
+          {showSearch && (
+            <div style={{ marginTop: '20px' }}>
+              <input
+                type="text"
+                placeholder="Search..."
+                style={{
+                  padding: '8px',
+                  fontSize: '16px',
+                  width: '200px',
+                  borderRadius: '4px',
+                  border: '1px solid #ccc',
+                }}
+              />
+            </div>
+          )}
+        </div>
+      );
+    }
     
-}
+    export default SearchBar;
