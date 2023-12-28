bdf1b01ba37715acf3aeec5646a5d64cad0641a3
Anshpreet Kaur <anshpreetlayal@outlook.com>
2023-12-28 11:10:08 -0500

    updating searchbar

--- Components/SearchBar.js
+++ Components/SearchBar.js
@@ -1,22 +1,5 @@
-import React, {useState} from "react";
+import React, {useState} from 'react';
 
-function SearchBar ({onSearch}) {
-    const [searchTerm, setSearchTerm] = useState('');
-
-    const handleSearch = ( ) => {
-        onSearch (searchTerm);
-    };
-
-    return (
-        <div> 
-            <input 
-            type = "text"
-            value={searchTerm}
-            onChange= {e => setSearchTerm(e.target.value)}
-            />
-            <button onClick={handleSearch}>Search</button>
-        </div>
-    );
+function SearchBar(){
+    const[showSearch, SetShowSearch] = useState(false);
 }
-
-export default SearchBar;
